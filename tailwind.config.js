c5a9c7e39e3512f7d9486a2b685c74b20576f33c
marina23garcia <marina_garciaaa@hotmail.com>
2022-01-13 09:52:30 +0100

    New changes - Ficha 6

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'xxs': '9px',
+      },
+      backgroundImage: {
+        'movil-pattern': "url('https://cdn.janto.es/test/pruebas/design/20220112105517_1641981317.4721backgrou.png')",
+        'movil-pattern-svg': "url('https://cdn.janto.es/test/pruebas/design/20220112111917_1641982757.1012backsvg.svg')",
+      },
+      colors: {
+        primary: '#563D9B',
+        titles: '#575757',
+      },
+    },
   },
   plugins: [],
 };
