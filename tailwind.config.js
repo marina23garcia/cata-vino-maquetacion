508a649f010403b4a618d0684fd2d7325ebfd664
marina23garcia <marina_garciaaa@hotmail.com>
2022-02-07 14:13:52 +0100

    Maquetacion de cabecera + Colores configuracion + Limpieza general de archivos anteriores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,25 @@ module.exports = {
   content: ["./**/*.html"],
   theme: {
     extend: {
-      fontSize: {
-        'xxs': '9px',
-      },
-      backgroundImage: {
-        'movil-pattern': "url('https://cdn.janto.es/test/pruebas/design/20220112105517_1641981317.4721backgrou.png')",
-        'movil-pattern-svg': "url('https://cdn.janto.es/test/pruebas/design/20220112111917_1641982757.1012backsvg.svg')",
-      },
       colors: {
-        primary: '#563D9B',
-        titles: '#575757',
+        primario: '#B28C00',
+        back: {
+          claro: '#FAFAFA',
+          oscuro: '#333333',
+        },
+        front: {
+          claro: '#FFFFFF',
+          oscuro: '#6A6F74'
+        },
+        accent: '#957614',
+        base: '#545760',
+        exito: '#00AB89',
+        error: '#C0564B',
+        info: '#00ADF2',
+        aviso: '#FFD330',
+
+        blanco: '#FFFFFF',
+        
       },
     },
   },
