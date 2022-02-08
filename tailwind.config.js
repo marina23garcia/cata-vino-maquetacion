d92777b8d479583ee5c501b28a606707d8501d27
marina23garcia <marina_garciaaa@hotmail.com>
2022-02-08 15:36:58 +0100

    ultimos cambios del dia 08/02 a las 15:30

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,20 @@ module.exports = {
         
       },
       backgroundImage: {
-        'bottles': "url('./img/bottles.png')",
+        'bottles': "url('/img/bottles.png')",
+        'copas': "url('/img/copas.jpg')",
+        'montilla': "url('/img/montilla-moriles.jpg')",
+      },
+      fontSize: {
+        'xxs': '.50rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
+      screens: {
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
       },
     },
   },
